6f89823c5326c38ad7a099d689451a93a85797cd
Maxim Nechaev <nechaev_maxim@mail.ru>
2019-06-14 15:01:11 +0300

    Create ch-2.pl

---
+++ 012/ch-2.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+# https://perlweeklychallenge.org/blog/perl-weekly-challenge-012/
+use strict;
+use feature 'say';
+use List::Util qw/all/;
+
+sub common_root {
+    my ($sep, @paths) = @_;
+
+    my $root = '';
+    foreach my $part ( split $sep, $paths[0] ) {
+        next unless $part;
+        last unless all { m/^${root}${sep}${part}($sep|$)/ } @paths;
+        $root .= $sep.$part;
+    }
+    return $root;
+}
+
+say common_root '/', ('/a/b/c/d', '/a/b/cd', '/a/b/cc', '/a/b/c/d/e');
